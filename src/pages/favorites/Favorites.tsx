722f95a8e748965b80ae0644488e5d5d8c6d4e1e
Julia Hu <40742100+jhukosh@users.noreply.github.com>
2023-06-14 09:35:13 +0200

    Update Favorites.tsx

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -3,5 +3,6 @@ import { FavoriteCardsList } from "./FavoriteCardsList";
 import "./Favorites.css";
 
 export const Favorites = () => {
+	// Here you could have called the context and filter the restaurants list to pass it as prop to CardsList
 	return <FavoriteCardsList />;
 };
